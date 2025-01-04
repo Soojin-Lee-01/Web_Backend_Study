9438b362a28558850c511662a7ff61ffdd628a0d
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-01-04 23:56:59 +0900

    Add : JPA 공부 update

--- Spring/0104/library-app/src/main/java/com/group/libraryapp/config/UserConfiguration.java
+++ Spring/0104/library-app/src/main/java/com/group/libraryapp/config/UserConfiguration.java
@@ -1,9 +1,6 @@
 package com.group.libraryapp.config;
 
-import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.repository.user.UserRepository;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class UserConfiguration {
--- Spring/0104/library-app/src/main/java/com/group/libraryapp/domain/user/User.java
+++ Spring/0104/library-app/src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,8 +1,29 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+
+// 저장되고, 관리되어야하는 데이터
+@Entity
 public class User {
+
+    // 이 필드를 primary key로 간주한다.
+    @Id
+    // primary key는 자동 생성되는 값이다.
+    @GeneratedValue(strategy  = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false, length = 20, name = "name") // name varchar(20)
     private String name;
+    // 생략 가능 ~ 조건이 같다면!
     private Integer age;
+
+    protected User(){
+
+    };
+
+    public Long getId() {
+        return id;
+    }
 
     public User(String name, Integer age) {
         if (name == null || name.isBlank()) {
@@ -18,5 +39,9 @@ public class User {
 
     public Integer getAge() {
         return age;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
     }
 }
