4020e6d42464b70bc38c5950735eb7bf11f6046b
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-01-04 17:19:58 +0900

    Add : Spring Boot 공부 update

---
+++ Spring/0104/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -0,0 +1,19 @@
+package com.group.libraryapp.controller.calculator;
+
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
+import org.springframework.web.bind.annotation.*;
+
+// API의 진입 지점
+@RestController
+public class CalculatorController {
+    @GetMapping("/add") // GET /add
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply") // POST /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
+    }
+}
